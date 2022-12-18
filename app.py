08e92def2ad0a72f637018991a022214332986a8
Nguyen Dinh Duy Thong <nguyendinhduythong@gmail.com>
2022-12-18 12:25:59 +0700

    init project: Upload and save file to server

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, jsonify, flash, redirect, url_for
+import time
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'static/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+app.secret_key = "thong1412"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16*1024*1024
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET'])
+def to_homepage():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def handle_image():
+    if 'file' not in request.files:
+        flash('No file part!')
+        return redirect(request.url)
+    file = request.files['file']
+
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Image successfully uploaded and calculate ...')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
